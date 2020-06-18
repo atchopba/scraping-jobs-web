3977fa7bec8d16b62e499cf252d97c2f1a1aba47
albinTch <atchopba@gmail.com>
2020-06-19 00:45:00 +0200

    Update __init__.py

--- scraping_jobs_web/__init__.py
+++ scraping_jobs_web/__init__.py
@@ -15,8 +15,6 @@ def create_app():
     #
     app = Flask(__name__, instance_relative_config=False)
     #
-    #app.config.from_object("config.Config")
-    #
     with app.app_context():
         from . import routes
         #
